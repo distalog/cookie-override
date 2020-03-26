319e555ce917a5753278d2befb6d1ea8cb38b972
didier <didier@dfr.ch>
2020-03-26 18:58:13 +0100

    Refactor into lib and example

--- example/app.js
+++ example/app.js
@@ -2,25 +2,7 @@
 
 const Hapi = require('@hapi/hapi');
 const Path = require('path');
-
-const cookiePostHandler = function (cookieRegex = "^cookie_", redirect = true) {
-    return function( cookieRegex, redirect, request, h ) {
-        if (request.method == "post") {
-            for(const k in request.payload) {
-                if (k.match(cookieRegex)) {
-                    const cookieName = k.replace(cookieRegex,"")
-                    const cookieValue = request.payload[k]
-                    h.state(cookieName,cookieValue)
-                }
-            }
-            if(redirect) {
-                return h.redirect(request.url).takeover()
-            }
-        }
-        return h.continue;
-    }.bind(undefined, cookieRegex, redirect);
-};
-
+const cookiePostHandler = require("../cookie-post-handler")
 const start = async () => {
 
     const server = Hapi.server({
